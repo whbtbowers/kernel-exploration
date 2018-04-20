1caff855f037bd8217e89518796dd0f9ada9cff3
whbtbowers <33463737+whbtbowers@users.noreply.github.com>
2018-04-20 16:55:32 +0100

    Add files via upload

---
+++ scripts/mltest/simtest1_6.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 16:18:08 2018
+
+@author: whb17
+"""
+
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import plotly.plotly as py
+import plotly.tools as tls
+tls.set_credentials_file(username='whtbowers', api_key='skkoCIGowBQdx7ZTJMzM')
+
+from sklearn.model_selection import StratifiedKFold, cross_val_score, KFold, cross_val_predict, cross_validate, train_test_split
+from sklearn.metrics import accuracy_score, roc_curve, auc
+from sklearn.preprocessing import label_binarize
+#from sklearn.decomposition import KernelPCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+from scipy import interp
+
+#Select current toy dataset
+dataset = '010'
+
+#Import toy data and target
+X = pd.read_csv('../../data/simulated/mvnsim/mvnsim' + dataset + '.csv', sep=',', header=0, index_col=0).as_matrix()
+y = np.load('../../data/simulated/mvnsim/target' + dataset + '.npy')
+
+cv = StratifiedKFold(n_splits=10, random_state=10)
+classifier = SVC(kernel='linear', probability=True)
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+plt.figure(figsize=(10, 10))
+i = 0
+for train, test in cv.split(X, y):
+    probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+    # Compute ROC curve and area the curve
+    fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+    tprs.append(interp(mean_fpr, fpr, tpr))
+    tprs[-1][0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    aucs.append(roc_auc)
+    plt.plot(fpr, tpr, lw=1, alpha=0.3,
+             label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+
+    i += 1
+plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+         label='Luck', alpha=.8)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr, mean_tpr, color='b',
+         label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+         lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                 label=r'$\pm$ 1 std. dev.')
+
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic (SVM only)')
+plt.legend(loc="lower right")
+plt.show()
